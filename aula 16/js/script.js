1828dee768fadddbbae086d1daafee646e50c2f8
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2020-09-30 17:09:06 -0300

    Usar async await

--- aula 16/js/script.js
+++ aula 16/js/script.js
@@ -1,10 +1,22 @@
 function start() {
+  //   doFetch();
+  doFetchAsync();
+}
+
+function doFetch() {
   // O .then() garante q uma função de callback executará se a requisição for bem sucedida
   fetch("https://api.github.com/users/vieira-gabriel").then((res) => {
     res.json().then((data) => {
       showData(data);
     });
   });
+}
+
+// Forma mais simples de escrever a função de cima
+async function doFetchAsync() {
+  const res = await fetch("https://api.github.com/users/vieira-gabriel");
+  const json = await res.json();
+  showData(json);
 }
 
 function showData(data) {
