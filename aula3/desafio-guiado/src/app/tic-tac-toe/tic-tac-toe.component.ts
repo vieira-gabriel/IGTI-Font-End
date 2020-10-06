7a0a150111a331e0619bc1c9222b8a7f7feb3548
Gabriel Vieira de Arimatéa <vieira.arimatea@gmail.com>
2020-10-06 15:27:38 -0300

    Concluir desafio

--- aula3/desafio-guiado/src/app/tic-tac-toe/tic-tac-toe.component.ts
+++ aula3/desafio-guiado/src/app/tic-tac-toe/tic-tac-toe.component.ts
@@ -7,10 +7,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicTacToeComponent {
   currentPlayer: string = 'O';
+  winner: string = '';
   board: string[][] = [
     ['', '', ''],
     ['', '', ''],
     ['', '', ''],
   ];
-  constructor() {}
+
+  processPlay(line: number, col: number) {
+    if (this.board[line][col] == '' && this.winner == '') {
+      this.board[line][col] = this.currentPlayer;
+
+      if (this.checkWinner(this.currentPlayer)) {
+        this.winner = this.currentPlayer;
+      }
+
+      if (this.currentPlayer == 'O') {
+        this.currentPlayer = 'X';
+      } else {
+        this.currentPlayer = 'O';
+      }
+    }
+  }
+
+  checkWinner(player: string): boolean {
+    for (let i = 0; i < this.board.length; i++) {
+      if (
+        this.board[i][0] == player &&
+        this.board[i][1] == player &&
+        this.board[i][2] == player
+      ) {
+        return true;
+      }
+      if (
+        this.board[0][i] == player &&
+        this.board[1][i] == player &&
+        this.board[2][i] == player
+      ) {
+        return true;
+      }
+    }
+
+    if (
+      this.board[0][0] == player &&
+      this.board[1][1] == player &&
+      this.board[2][2] == player
+    ) {
+      return true;
+    }
+    if (
+      this.board[0][2] == player &&
+      this.board[1][1] == player &&
+      this.board[2][0] == player
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  reset() {
+    this.currentPlayer = 'O';
+    this.winner = '';
+    this.board = [
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ];
+  }
 }
